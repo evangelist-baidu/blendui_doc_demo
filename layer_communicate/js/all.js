d5b8eb9b3143cf2689b155e87073ed15658dd0b7
yunlong <yunlong@baidu.com>
2014-11-13 13:57:54 +0800

    统一引入blendUI方式

--- layer_communicate/js/all.js
+++ layer_communicate/js/all.js
@@ -1,16 +1,22 @@
+//Blend.lightInit({
+//    ak:'XkC0UKL8Z0cEP5zqKHuc8lIt', //轻应用apikey，请参考《获取API Key》文档
+//    module:["blendui"]//根据需要添加模块到数组中即可
+//});
+
 document.addEventListener("blendready", function () {
     var main = Blend.ui;
-
+    console.log('blendready');
     Blend.ui.layerInit("0", function (dom) {
         var subLayers = [{id:'subLayerId',url:'item.html','active':true}];
 
-        var tabs = new main.LayerGroup({
+        new main.LayerGroup({
             layers: subLayers,
             left: 0,
             top: 200
         });
 
-        $('#notifyBtn',dom).click(function(e){
+        $('#notifyBtn',dom).on("click",function(e){
+            alert('click!');
             var msgContent = $('#input_msg').val();
             Blend.ui.fire("sendMsg", "subLayerId",{content:msgContent});
         });
